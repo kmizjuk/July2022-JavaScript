fa559dd3ee098bbae6100dd79b7fb98e52a616b6
Khrystyna <kmizjuk@gmail.com>
2022-07-23 15:51:11 +0300

    Homework 1 - done.

---
+++ Homework1/JShw1.js
@@ -0,0 +1,56 @@
+let a = 'Hello';
+let b = 'owu';
+let c = 'com';
+let d = '.ua';
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let b1 = true;
+let b2 = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(b1);
+console.log(b2);
+console.log(a,b,c,d,);
+
+alert(a+" "+b+" "+c+" "+d);
+alert(n1+n2+n3+n4+n5+n6+n7);
+alert(b1);
+alert(b2);
+
+document.write(a +" "+b+" "+c+d);
+document.write(n1+n2+n3+n4+n5+n6+n7);
+document.write(b1+b2);
+
+let firtName = "Miziuk";
+let middleName = 'Khrystyna';
+let lastName = 'Yaroslavivna';
+let PIB = firtName+" "+middleName+" "+lastName;
+console.log(PIB);
+
+let a2 = 100;
+let b3 = '100';
+let c2 = true;
+console.log(a2,typeof a2);
+console.log(b3,typeof b3);
+console.log(c2,typeof c2);
+let N = prompt("First Name");
+let N2 = prompt("Last Name");
+let A = prompt("Your age");
+console.log(N,N2,A);
+
+
